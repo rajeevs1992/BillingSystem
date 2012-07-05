0ff28bbe1d5d5d675c2769b7fec8118355862f39
Rajeev S <rajeevs1992@gmail.com>
2012-07-05 23:52:32 +0530

    latest schema

--- docs/db.sql
+++ docs/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 19, 2012 at 12:19 AM
+-- Generation Time: Jul 05, 2012 at 11:50 PM
 -- Server version: 5.1.54
 -- PHP Version: 5.3.5-1ubuntu7.4
 
@@ -46,18 +46,12 @@ CREATE TABLE IF NOT EXISTS `invoices` (
 --
 
 REPLACE INTO `invoices` (`slNo`, `mrp`, `PplusT`, `name`, `code`, `qty`, `unitPrice`, `rateOfTax`, `taxAmt`, `cess`, `total`, `billNo`, `user`) VALUES
-(1, 3, 3.12, 'Pencil Apsara', '001', 15, 3, 4, 1.8, 0.001, 46.8, '1', NULL),
-(2, 12, 13.5, 'eraser', 'abc', 15, 12, 12.5, 22.5, 0.015, 202.5, '1', NULL),
-(3, 5, 5, 'lexi', '101', 5, 5, 0, 0, 0, 25, '1', NULL),
-(1, 0, 9.994, 'Book-Theodolite field book', 'b008', 1, 9.61, 4, 0.384, 0.004, 9.994, '2', NULL),
-(2, 0, 4.995, 'Arrows', 'a003', 6, 4.44, 12.5, 3.33, 0.006, 29.97, '2', NULL),
-(3, 0, 119.954, 'Box Sigma Instrument', 'b010', 1, 115.34, 4, 4.614, 0.046, 119.954, '2', NULL),
-(1, 0, 9.994, 'Book-Theodolite field book', 'b008', 15, 9.61, 4, 5.766, 0.004, 149.91, '3', NULL),
-(2, 0, 4.995, 'Arrows', 'a003', 15, 4.44, 12.5, 8.325, 0.006, 74.925, '3', NULL),
-(1, 0, 9.994, 'Book-Theodolite field book', 'b008', 1, 9.61, 4, 0.384, 0.004, 9.994, '4', NULL),
-(2, 0, 4.995, 'Arrows', 'a003', 1, 4.44, 12.5, 0.555, 0.006, 4.995, '4', NULL),
-(1, 0, 9.994, 'Book-Theodolite field book', 'b008', 100, 9.61, 4, 38.44, 0.004, 999.4, '5', NULL),
-(1, 0, 9.994, 'Book-Theodolite field book', 'b008', 15, 9.61, 4, 5.766, 0.004, 149.91, '6', 'rajeev');
+(1, 0, 9.994, 'Book-Theodolite field book', 'b008', 10, 9.61, 4, 3.844, 0.004, 99.94, '1', 'rajeev'),
+(2, 10, 12.48, 'Pen', 'abc', 1, 12, 4, 0.48, 0.005, 12.48, '1', 'rajeev'),
+(1, 150, 148, 'BSD', 'absd', 8, 148, 0, 0, 0, 1184, '2', 'rajeev'),
+(1, 0, 9.994, 'Book-Theodolite field book', 'b008', 1, 9.61, 4, 0.384, 0.004, 9.994, '3', 'rajeev'),
+(2, 150, 148, 'BSD', 'absd', 8, 148, 0, 0, 0, 1184, '3', 'rajeev'),
+(3, 0, 119.954, 'Box Sigma Instrument', 'b010', 1, 115.34, 4, 4.614, 0.046, 119.954, '3', 'rajeev');
 
 -- --------------------------------------------------------
 
@@ -84,9 +78,12 @@ CREATE TABLE IF NOT EXISTS `item` (
 --
 
 REPLACE INTO `item` (`code`, `name`, `mrp`, `unitPrice`, `rateOfTax`, `sellingPrice`, `purchasingPrice`, `profitPerUnit`, `totalStock`, `openingStock`) VALUES
-('b008', 'Book-Theodolite field book', NULL, 9.61, 4, 10, 8, 10, 4, 287),
-('a003', 'Arrows', NULL, 4.44, 12.5, 5, NULL, 5, 8, 15),
-('b010', 'Box Sigma Instrument', NULL, 115.34, 4, 120, NULL, 120, 119, 149);
+('b008', 'Book-Theodolite field book', NULL, 9.61, 4, 10, 8, 10, 2, 287),
+('a003', 'Arrows', NULL, 4.44, 12.5, 5, NULL, 5, 10, 15),
+('b010', 'Box Sigma Instrument', NULL, 115.34, 4, 120, NULL, 120, 93, 149),
+('b0081', 'Pen1', 10, 9.61, 4, 9.994, 12, -2.006, 14, 0),
+('absd', 'BSD', 150, 148, 0, 148, 145, 3, 4, 0),
+('abc', 'Pen', 10, 12, 4, 12.48, 12, 0.48, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -104,14 +101,6 @@ CREATE TABLE IF NOT EXISTS `purchase` (
 -- Dumping data for table `purchase`
 --
 
-REPLACE INTO `purchase` (`code`, `qty`, `date`) VALUES
-('b008', 15, '2012-06-18'),
-('b008', 15, '2012-06-18'),
-('b008', 15, '2012-06-18'),
-('b008', 15, '2012-06-18'),
-('b008', 15, '2012-06-18'),
-('b008', 15, '2012-06-18'),
-('a003', 15, '2012-06-18');
 
 -- --------------------------------------------------------
 
@@ -139,12 +128,27 @@ CREATE TABLE IF NOT EXISTS `sales` (
 --
 
 REPLACE INTO `sales` (`billNo`, `date`, `salesNonTax`, `sales4pcTax`, `sales125pcTax`, `tax4pc`, `tax125pc`, `cess4pc`, `cess125pc`, `totalWithoutTax`, `cashOrCredit`) VALUES
-('1', '2012-05-30', 25, 46.8, 202.5, 1.8, 22.5, 0.001, 0.015, 274.3, 'C'),
-('2', '2012-06-13', 0, 129.948, 29.97, 4.998, 3.33, 0.05, 0.006, 159.918, 'C'),
-('3', '2012-06-18', 0, 149.91, 74.925, 5.766, 8.325, 0.004, 0.006, 224.835, 'C'),
-('4', '2012-06-18', 0, 9.994, 4.995, 0.384, 0.555, 0.004, 0.006, 14.989, 'C'),
-('5', '2012-06-18', 0, 0, 0, 0, 0, 0, 0, 0, 'C'),
-('6', '2012-06-19', 0, 149.91, 0, 5.766, 0, 0.004, 0, 149.91, 'C');
+('1', '2012-07-05', 0, 112.42, 0, 4.324, 0, 0.009, 0, 112.42, 'C'),
+('2', '2012-07-05', 1184, 0, 0, 0, 0, 0, 0, 1184, 'C'),
+('3', '2012-07-05', 1184, 129.948, 0, 4.998, 0, 0.05, 0, 1313.95, 'C');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp`
+--
+
+CREATE TABLE IF NOT EXISTS `temp` (
+  `tempBillNo` int(11) DEFAULT NULL,
+  `code` varchar(5) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `Date` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `temp`
+--
+
 
 -- --------------------------------------------------------
 
@@ -166,5 +170,3 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 REPLACE INTO `users` (`uname`, `passwd`, `email`, `acessLevel`) VALUES
 ('rajeev', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'rajeevs1992@gmail.com', 3);
-
-create table temp (tempBillNo INT(11),code varchar(5),qty int(11));
