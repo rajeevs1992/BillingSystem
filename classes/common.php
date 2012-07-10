53082046523504a3be810846316d498a51e74768
Paul Davies C <pauldaviesc@gmail.com>
2012-07-10 19:24:06 +0530

    Control panel implemented. Made changes to classes/common.php

--- classes/common.php
+++ classes/common.php
@@ -22,11 +22,7 @@
 				<tr>";
 			echo sprintf($buttonLink,"/views/bill.php","New Bill");
 			echo sprintf($buttonLink,"/views/showSavedBills","Saved Bills");
-			echo sprintf($buttonLink,"/views/addItem.php","Add new item");
-			echo sprintf($buttonLink,"/","Delete item");
-			echo sprintf($buttonLink,"/views/monthlyReport.php","Monthly Statement");
-			echo sprintf($buttonLink,"/views/fullStockData.php","Full Stock");
-			echo sprintf($buttonLink,"/views/purchase.php","Purchase");
+			echo sprintf($buttonLink,"/views/controlPanel.php","Control Panel");
 			echo sprintf($buttonLink,"/controllers/logout.php","Logout");
 			echo "</tr></table>";	
 		}
@@ -42,7 +38,36 @@
 		
 		
 	};
+	class controlPanelPage
+	{
+		public function __construct($title,$level)
+		{
+			error_reporting(E_ALL);
+			session_start();
+			if((!isset($_SESSION['uname'])) || $_SESSION['acessLevel']<$level)
+			{
+				header("location:/views/login.php");
+			}
+			$buttonLink="<th><a href='%s' ><input type=submit value='%s' style=height:40px;width:150px></a></th>";
+			$header="
+				<html>
+				<font face=ubuntu>
+				<title>$title</title>
+				<h1><center>$title</center></h1>
+				<br>
+				<body style='background:url(/views/images/gr1.jpg)' vlink=blue>";
+			echo "$header
+				<table border=1>
+				<tr>";
+			echo sprintf($buttonLink,"/views/bill.php","New Bill");
+			echo sprintf($buttonLink,"/views/showSavedBills","Saved Bills");
+			echo sprintf($buttonLink,"/views/addItem.php","Add new item");
+			echo sprintf($buttonLink,"/","Delete item");
+			echo sprintf($buttonLink,"/views/monthlyReport.php","Monthly Statement");
+			echo sprintf($buttonLink,"/views/fullStockData.php","Full Stock");
+			echo sprintf($buttonLink,"/views/purchase.php","Purchase");
+			echo sprintf($buttonLink,"/controllers/logout.php","Logout");
+			echo "</tr></table>";	
+		}
+	};
 ?>
-
-
-
---
+++ views/controlPanel.php
@@ -0,0 +1,4 @@
+<?php
+	require_once("$_SERVER[DOCUMENT_ROOT]/classes/common.php");
+	$page=new controlPanelPage("Engineering College Co-Operative Society Ltd,R-51",1);
+?>
