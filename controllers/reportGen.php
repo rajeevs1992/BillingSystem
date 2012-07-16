184664e0d1864ecfad997c09f940a32106f67e7e
Rajeev S <rajeevs1992@gmail.com>
2012-07-16 22:52:41 +0530

    report generation for purchase also

--- controllers/reportGen.php
+++ controllers/reportGen.php
@@ -10,12 +10,28 @@
 	else
 	{
 		$var=date("FY", mktime(0, 0, 0, (date('m')-1))); 
-		$file_name="$_SERVER[DOCUMENT_ROOT]/reports/$var.csv";
+		$file_name="$_SERVER[DOCUMENT_ROOT]/reports/sales/$var.csv";
 		$handle=fopen($file_name,"w");
 		$heads="BILL NO,DATE,NON-TAXABLE SALES,AMT-$tax1%,AMT-$tax2%,TAX-$tax1%,TAX-$tax2%,TOTAL WITHOUT TAX,CASH-CREDIT\n";
 		fwrite($handle,$heads);
 		$headNos="1,2,3,4,5,6,7,8(3+4+5),9\n\n";
 		fwrite($handle,$headNos);
+		while($row=mysql_fetch_array($reply,MYSQL_NUM))
+		{
+			$str='';
+			foreach($row as &$tmp)
+			{
+				$str=$str."$tmp,";
+			}
+			$str=$str."\n";
+			fwrite($handle,$str);
+		}
+		fclose($handle);
+		$file_name="$_SERVER[DOCUMENT_ROOT]/reports/purchases/$var.csv";
+		$handle=fopen($file_name,"w");
+		$reply=$con->query("SELECT inv_no,date,name,qty,rot,pp,taxAmt,`from` FROM purchase WHERE MONTH(date)=$m");
+		$heads="INVOICE NO,DATE,ITEM NAME,QUANTITY,RATE OF TAX,PURCHASE PRICE,TAX PAID,PURCHASED FROM\n";
+		fwrite($handle,$heads);
 		while($row=mysql_fetch_array($reply,MYSQL_NUM))
 		{
 			$str='';
